93ec1a206cf8360c5997df9c4252cbdf38c718b2
MUQARRAB HUSSAIN <116738011+muqarrab469@users.noreply.github.com>
2023-07-11 23:42:14 +0500

    Back End Main Activity to Show Surahs

---
+++ MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.thequran;
+
+import static android.os.Build.VERSION_CODES.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.content.Intent;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> arrayList = new ArrayList<>();
+    ListView surahList;
+    //TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        QDH dataQuran=new QDH();
+        String[] surahNames = dataQuran.urduSurahNames;
+        surahList = findViewById(R.id.surah_List);
+        for(int i=0;i<surahNames.length;i++)
+        {
+            arrayList.add(surahNames[i]);
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,arrayList);
+        surahList.setAdapter(arrayAdapter);
+
+        surahList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, SurahText.class);
+                intent.putExtra ("surahNum", position);
+                intent.putExtra("surahName",surahNames[position]);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
